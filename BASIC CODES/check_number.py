2983c3783179752b69e36278f8a579c0a59fe77a
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-04-12 15:52:41 +0530

    basic

---
+++ BASIC CODES/check_number.py
@@ -0,0 +1,7 @@
+x=int(input("enter two numbers " ))
+if x>0:
+    print(x,"is possitive")
+elif x<0:
+    print(x,"is neagative")
+else:
+    print(x,"is neither negative nor positive ")
---
+++ BASIC CODES/find_large.py
@@ -0,0 +1,8 @@
+x=int(input("enter first number"))
+y=int(input("enter second number "))
+if x>y:
+    print(x ,"is grater than ",y)
+elif y>x:
+    print(y ,"is grater than ",x)
+else: 
+    print (x,y,"both are equal")
