d0ebedad4a3791f8a9a1313478cde6f7506f207f
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-04-14 01:42:47 +0530

    Delete largest3.py

--- largest3.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first A= "))
-b = int(input("Enter second B= "))
-c = int(input("Enter third C= "))
-if a > b and a > c:
-    print("A is largest")
-elif b > c:
-    print("B is largest")
-else:
-    print("C is largest")
