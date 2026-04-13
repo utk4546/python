d4cd0f79293db2723de6b46ffffa8a336389e893
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-04-14 01:43:18 +0530

    basic

---
+++ BASIC CODES/factorial.py
@@ -0,0 +1,11 @@
+num=int(input("enter a number = "))
+fact=1
+
+# for i in range(num,0,-1): 
+#     fact=fact*i
+# print("factorial",fact)
+
+for i in range(1,num+1):
+    fact=fact*i
+print("factorial",fact)
+    
