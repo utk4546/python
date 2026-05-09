bdc48891e0ccec5048860bf57cc29d118d1adbd9
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-10 01:44:54 +0530

    tuple

---
+++ tuple/0.py
@@ -0,0 +1,8 @@
+# creating a tuple
+numbers = (10, 20, 30, 40)#= 10,20,30,40
+
+print("Tuple:", numbers)
+
+# accessing elements
+print("First Element:", numbers[0])
+print("Third Element:", numbers[2])
