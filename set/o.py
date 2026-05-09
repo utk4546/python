16c5216e0ccdc5093c987f81195563ab59a6569a
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-10 01:46:22 +0530

    set

---
+++ set/o.py
@@ -0,0 +1,22 @@
+# Python Set
+# Definition
+
+# A Set in Python is a collection data type used to store multiple unique values in a single variable. Sets are unordered and do not allow duplicate values.
+
+# Features of Set
+# Unordered collection
+# No duplicate values allowed
+# Mutable (can be changed)
+# Fast searching operations
+# creating a set
+s = {1, 2, 3, 2, 1}
+
+print("Original Set:", s)
+
+# adding element
+s.add(5)
+
+# removing element
+s.remove(2)
+
+print("Updated Set:", s)
