206994e27af951600b149a119ba4e6ef0009caaa
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-14 14:34:47 +0530

    opps

---
+++ OOPs/1.py
@@ -0,0 +1,19 @@
+class student():
+    x=5
+    y=6
+    z=9
+    def fun(self,h):
+        print(h,self)
+        
+        
+        pass
+    def fun1():
+        pass
+    
+s1=student();
+# s1.fun()
+print(student.__dict__)
+student.fun(77,56)
+
+
+    
